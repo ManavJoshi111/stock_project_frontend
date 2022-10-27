40ec058d491aa6373ec2757e1e7a3c48ff0f2806
ManavJoshi <manavjoshi154@gmail.com>
2022-10-27 13:14:44 +0530

    Table Fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,54 +16,34 @@ const Header = () => {
     });
     useEffect(() => {
         CheckUser();
-        // const ws = new WebSocket(`wss://stream.binance.com:9443/ws`);
-        // ws.onopen = () => {
-        //     ws.send(JSON.stringify({
-        //         method: 'SUBSCRIBE',
-        //         params: ["shibusdt@trade", "ethbtc@trade", "bnbbusd@trade", "aptbusd@trade", "ethbusd@trade", "dfbusd@trade", "xrpusdt@trade", "dogeusdt@trade"],
-        //         id: 1,
-        //     }));
-        // }
-        // ws.onmessage = (e) => {
-        //     const dataJson = JSON.parse(e.data);
-        //     if (dataJson.s)
-        //         setPrice(
-        //             (prevPrize) => {
-        //                 // dataJson.p and prevPrize[dataJson.s] to be compared
-        //                 if (dataJson.p > prevPrize[dataJson.s]) {
-        //                     setColor({ ...color, [dataJson.s.toLowerCase()]: "green" });
-        //                 }
-        //                 else if (dataJson.p < prevPrize[dataJson.s]) {
-        //                     setColor({ ...color, [dataJson.s.toLowerCase()]: "red" });
-        //                 }
-        //                 return { ...prevPrize, [dataJson.s]: parseFloat(dataJson.p).toFixed(2) }
-        //             }
-        //         );
-        // }
         const ws = new WebSocket(`wss://stream.binance.com:9443/ws`);
         ws.onopen = () => {
             ws.send(JSON.stringify({
                 method: 'SUBSCRIBE',
-                params: ["shibusdt@trade", "ethbtc@trade", "bnbbusd@trade", "aptbusd@trade", "ethbusd@trade", "dfbusd@trade", "xrpusdt@trade", "dogeusdt@trade"],
+                params: ["btcusdt@trade", "ethusdt@trade", "bnbusdt@trade", "xrpusdt@trade", "busdusdt@trade", "adausdt@trade", "solusdt@trade", "dogeusdt@trade", "maticusdt@trade", "dotusdt@trade", "stethusdt@trade", "shibusdt@trade"],
                 id: 1,
+                interval: "SECONDS",
+                intervalNum: "5"
             }));
         }
-        ws.onmessage = (e) => {
-            const dataJson = JSON.parse(e.data);
-            if (dataJson.s)
-                setPrice(
-                    (prevPrize) => {
-                        // dataJson.p and prevPrize[dataJson.s] to be compared
-                        if (dataJson.p > prevPrize[dataJson.s]) {
-                            setColor({ ...color, [dataJson.s.toLowerCase()]: "green" });
+        setTimeout(() => {
+            ws.onmessage = (e) => {
+                const dataJson = JSON.parse(e.data);
+                if (dataJson.s)
+                    setPrice(
+                        (prevPrize) => {
+                            // dataJson.p and prevPrize[dataJson.s] to be compared
+                            if (dataJson.p > prevPrize[dataJson.s]) {
+                                setColor({ ...color, [dataJson.s.toLowerCase()]: "green" });
+                            }
+                            else if (dataJson.p < prevPrize[dataJson.s]) {
+                                setColor({ ...color, [dataJson.s.toLowerCase()]: "red" });
+                            }
+                            return { ...prevPrize, [dataJson.s]: parseFloat(dataJson.p).toFixed(2) }
                         }
-                        else if (dataJson.p < prevPrize[dataJson.s]) {
-                            setColor({ ...color, [dataJson.s.toLowerCase()]: "red" });
-                        }
-                        return { ...prevPrize, [dataJson.s]: parseFloat(dataJson.p).toFixed(2) }
-                    }
-                );
-        }
+                    );
+            }
+        }, 2000);
     }, []);
 
     return (
@@ -73,6 +53,7 @@ const Header = () => {
                 <table className="table">
                     <thead>
                         <tr>
+                            <th scope="col">Sr No.</th>
                             <th scope="col">Symbol</th>
                             <th scope="col">Price</th>
                             <th scope="col">Graph</th>
@@ -85,9 +66,11 @@ const Header = () => {
                                 return (
                                     <>
                                         <tr key={index}>
-                                            <td>{key}</td>
-                                            <td className={color[key.toLowerCase()]}>{price[key]}</td>
-                                            <td style={{ textAlign: "center" }}><Chart rs={price[key]} title={key} /></td>
+                                            <td className='w-25'>{index + 1}</td>
+                                            <td className='w-25'>{key}</td>
+                                            <td className={color[key.toLowerCase()] + " w-25"}>{price[key]}</td>
+                                            {/* <Chart rs={price[key]} title={key} /> */}
+                                            <td style={{ textAlign: "center" }} className="w-25"></td>
                                         </tr>
                                     </>
 
