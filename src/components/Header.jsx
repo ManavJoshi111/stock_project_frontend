c7207a98ce9028e7c9b417b5e3e28b41eacd555d
ManavJoshi <manavjoshi154@gmail.com>
2024-05-25 20:09:53 +0530

    fix: change coingecko api currency to usd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     try {
       setLoading(true);
       const response = await fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       );
       const fetchedData = await response.json();
       const filteredData = fetchedData.filter(
